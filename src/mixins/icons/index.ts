2df0731c2d825057842cae13740417ab4d315a07
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-11 12:09:24 +0200

    setup stack icons importing

---
+++ src/mixins/icons/index.ts
@@ -0,0 +1,44 @@
+// Import all stack SVG files
+import icon17 from '@/assets/icons/D3.svg'
+import icon1 from '@/assets/icons/api.svg'
+import icon2 from '@/assets/icons/bootstrap.svg'
+import icon3 from '@/assets/icons/css.svg'
+import icon19 from '@/assets/icons/devops.svg'
+import icon15 from '@/assets/icons/docker.svg'
+import icon13 from '@/assets/icons/figma.svg'
+import icon4 from '@/assets/icons/git.svg'
+import icon5 from '@/assets/icons/github.svg'
+import icon6 from '@/assets/icons/js.svg'
+import icon20 from '@/assets/icons/mui.svg'
+import icon7 from '@/assets/icons/mySQL.svg'
+import icon18 from '@/assets/icons/netlify.svg'
+import icon8 from '@/assets/icons/next.svg'
+import icon9 from '@/assets/icons/nmp.svg'
+import icon12 from '@/assets/icons/node.svg'
+import icon14 from '@/assets/icons/postman.svg'
+import icon10 from '@/assets/icons/react.svg'
+import icon11 from '@/assets/icons/sass.svg'
+import icon16 from '@/assets/icons/vercel.svg'
+// Export all SVG files
+export const svgFiles = {
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+  icon6,
+  icon7,
+  icon8,
+  icon9,
+  icon10,
+  icon11,
+  icon12,
+  icon13,
+  icon14,
+  icon15,
+  icon16,
+  icon17,
+  icon18,
+  icon19,
+  icon20
+}
