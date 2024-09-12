111d9f8023b05c6e218e76b82dec4d079d74c170
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-12 21:10:45 +0200

    define theming

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,37 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { reactive } from 'vue'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+const themes = {
+  light: {
+    primary: '#FFFFFF',
+    secondary: '#F0F0F0',
+    text: '#000000'
+  },
+  dark: {
+    primary: '#333333',
+    secondary: '#222222',
+    text: '#FFFFFF'
   }
+}
 
-  return { count, doubleCount, increment }
+export const useThemeStore = defineStore('themeStore', {
+  state: () => ({
+    currentTheme: reactive(themes.light),
+    themes
+  }),
+  actions: {
+    detectSystemTheme() {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+      this.currentTheme = prefersDark ? this.themes.dark : this.themes.light
+
+      // Add listener to detect changes in the system theme
+      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+        this.currentTheme = e.matches ? this.themes.dark : this.themes.light
+      })
+    },
+    toggleTheme() {
+      this.currentTheme =
+        this.currentTheme === this.themes.light ? this.themes.dark : this.themes.light
+    }
+  }
 })
