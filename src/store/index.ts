247e8bf42b38bd8c26b859ef02d7bda3320f5901
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-13 14:28:18 +0200

    update theme

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
 import { defineStore } from 'pinia'
-import { reactive } from 'vue'
+import { onMounted, reactive } from 'vue'
 
 const themes = {
   light: {
-    primary: '#FFFFFF',
-    secondary: '#F0F0F0',
-    text: '#000000'
+    primary: '#E0D7D7',
+    secondary: '#C9B8B8',
+    text: '#473131'
   },
   dark: {
-    primary: '#333333',
-    secondary: '#222222',
-    text: '#FFFFFF'
+    primary: '#473131',
+    secondary: '#473131',
+    text: '#86d9ca'
   }
 }
 
@@ -27,11 +27,28 @@ export const useThemeStore = defineStore('themeStore', {
       // Add listener to detect changes in the system theme
       window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
         this.currentTheme = e.matches ? this.themes.dark : this.themes.light
+        this.saveThemeToSessionStorage() // Save theme when system theme changes
       })
     },
     toggleTheme() {
       this.currentTheme =
         this.currentTheme === this.themes.light ? this.themes.dark : this.themes.light
+      this.saveThemeToSessionStorage() // Save theme when toggled manually
+    },
+    saveThemeToSessionStorage() {
+      sessionStorage.setItem('theme', this.currentTheme === this.themes.light ? 'light' : 'dark')
+    },
+    loadThemeFromSessionStorage() {
+      const storedTheme = sessionStorage.getItem('theme')
+      if (storedTheme) {
+        this.currentTheme = storedTheme === 'light' ? this.themes.light : this.themes.dark
+      }
     }
   }
+})
+
+onMounted(() => {
+  const themeStore = useThemeStore()
+  themeStore.loadThemeFromSessionStorage()
+  themeStore.detectSystemTheme()
 })
