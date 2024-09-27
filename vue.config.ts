6a124463754c223186cd87bf52c7ab2cf7ff65b2
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-27 16:49:09 +0200

    build changes

--- vue.config.ts
+++ vue.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/portfolio/'
+  base: '/portfolio/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
