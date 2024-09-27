cb6d0384cd1ada4d2597701e23a45282e7194293
KatieLisabeth <kat.lisabeth@gmail.com>
2024-09-27 20:46:08 +0200

    deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,25 +9,11 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@/stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
-      '@/components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@/utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-      '@/controllers': fileURLToPath(new URL('./src/controllers', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // This helps split your app into chunks for better performance in production
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
-          }
-        }
-      }
-    }
+    sourcemap: true
   }
 })
